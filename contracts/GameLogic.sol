6547540e67477d507db698d0a9e8545122cd3156
Log <91562887+LogETH@users.noreply.github.com>
2022-03-08 22:56:55 -0800

    Added deaths

--- contracts/GameLogic.sol
+++ contracts/GameLogic.sol
@@ -9,18 +9,24 @@ contract GameLogic {
 
     Stat getstats;
     CombatContract combat;
+    Movement move;
     uint GameIDNonce;
+
+////Nested mappings are bruh, like, very bruh
 
     mapping(address => mapping(address => uint)) Invite;
     mapping(uint => mapping(address => mapping(uint => uint))) Pieces;
-    mapping(uint => mapping(address => mapping(uint => mapping(uint => uint)))) LocationX;
-    mapping(uint => mapping(address => mapping(uint => mapping(uint => uint)))) LocationY;
+    mapping(uint => mapping(address => mapping(uint => uint))) LocationX;
+    mapping(uint => mapping(address => mapping(uint => uint))) LocationY;
+    mapping(uint => mapping(address => mapping(uint => bool))) Dead;
     mapping(uint => Map) GameMap;
     mapping(address => bool) ingame;
 
 ////Mobile Stats
 
-    mapping(uint => mapping(address => mapping(uint => mapping(uint => uint)))) CurrentHP;
+    mapping(uint => mapping(address => mapping(uint => uint))) CurrentHP;
+
+////and the code
 
     function InviteOpponent(address Opponent, Map EnterMapAddress, uint Piece1, uint Piece2, uint Piece3, uint Piece4) external{
 
@@ -49,18 +55,31 @@ contract GameLogic {
         ingame[Opponent] = true;
     }
 
+//// Dev notes: AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+
     function PlayTurn(uint GameID, uint PieceID, uint MoveHowManySpacesX, uint MoveHowManySpacesY, bool Attack, uint AttackWhom) external {
 
-        require (Pieces[GameID][msg.sender][1] == PieceID || Pieces[GameID][msg.sender][2] == PieceID || Pieces[GameID][msg.sender][3] == PieceID || Pieces[GameID][msg.sender][4] == PieceID, "That Piece is not currenty being used in the game!");
+        require (Pieces[GameID][msg.sender][1] == PieceID || Pieces[GameID][msg.sender][2] == PieceID || Pieces[GameID][msg.sender][3] == PieceID || Pieces[GameID][msg.sender][4] == PieceID, "That Piece is not currently being used in the game!");
+        require (GameID < (GameIDNonce - 1), "That game doesn't even exist yet you idiot!");
+        require (Dead[GameID][msg.sender][PieceID] = false, "You can't move a dead piece.")
         
-
+        (LocationX[GameID][msg.sender][PieceID], LocationY[GameID][msg.sender][PieceID]) = move.MovePiece(PieceID, MoveHowManySpacesX, MoveHowManySpacesY);
 
         if(Attack == true){
 
-            uint HP1;
-            uint HP2;
+            require(move.getdistance(LocationX[GameID][msg.sender][PieceID],LocationY[GameID][msg.sender][PieceID],LocationX[GameID][msg.sender][AttackWhom],LocationY[GameID][msg.sender][AttackWhom]) == getstats.range(PieceID));
 
-            (HP1, HP2) = combat.fight(PieceID, AttackWhom);
+            (CurrentHP[GameID][msg.sender][PieceID], CurrentHP[GameID][msg.sender][AttackWhom]) = combat.fight(PieceID, AttackWhom);
+
+            if(CurrentHP[GameID][msg.sender][PieceID] = 0){
+
+                Dead[GameID][msg.sender][PieceID] = true;
+            }
+
+            if(CurrentHP[GameID][msg.sender][AttackWhom] = 0){
+
+                Dead[GameID][msg.sender][AttackWhom] = true;
+            }
         }
     }
 
@@ -70,7 +89,7 @@ contract GameLogic {
 ////                     Internal functions this contract uses                        ////
 //////////////////////////////////////////////////////////////////////////////////////////
 
-    function GetGameData(uint GameID, uint DataID) internal {
+    function GetLocation(uint GameID, uint DataID) internal {
 
 
 
@@ -82,6 +101,12 @@ contract GameLogic {
 
 
 
+}
+
+interface Movement{
+
+    function MovePiece(uint, uint, uint) external returns(uint,uint);
+    function getdistance(uint, uint, uint, uint) external returns (uint);
 }
 
 interface CombatContract{
@@ -101,4 +126,5 @@ interface Stat{
     function spd(uint256 PieceID) external returns(uint256);
     function maj(uint256 PieceID) external returns(uint256);
     function mhp(uint256 PieceID) external returns(uint256);
+    function range(uint PieceID) external returns(uint256);
 }
