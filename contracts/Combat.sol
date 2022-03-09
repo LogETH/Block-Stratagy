0f1734d891653ea3cbafe72f268894aa3f186c02
Log <91562887+LogETH@users.noreply.github.com>
2022-03-08 19:04:42 -0800

    Update Combat.sol

--- contracts/Combat.sol
+++ contracts/Combat.sol
@@ -7,15 +7,49 @@ contract combat {
     //This contract stores combat logic, nothing else. It is called whenever there is combat.
 
     Stat getstats;
+    Game getdata;
 
     function fight(uint Piece1, uint Piece2) external returns (uint){
 
         uint Damage;
-        uint HP1;
-        uint HP2;
+        uint HP1 = getdata.hp(Piece1);
+        uint HP2 = getdata.hp(Piece2);
 
+        this.EnablePreCombatSkills();
         
-        Damage = getstats.atk(Piece1) - getstats.def(Piece2);
+        this.Attack(Piece1, Piece2);
+        if(HP2 <= 0){return 1;}
+        this.Attack(Piece2, Piece1);
+        if(HP2 <= 0){return 1;}
+
+        if((getstats.spd(Piece1) + 10) > getstats.spd(Piece2)){
+ 
+            this.Attack(Piece1, Piece2);
+            if(HP2 <= 0){return 1;}
+        }
+        if((getstats.spd(Piece2) + 10) > getstats.spd(Piece1)){
+ 
+            this.Attack(Piece2, Piece1);
+            if(HP2 <= 0){return 1;}
+        }
+
+
+    }
+
+    function Attack(uint Initiator, Defender) external {
+
+                Damage = getstats.atk(Initiator) - getstats.def(Defender);
+
+                this.EnableInCombatSkills()
+
+                if(Damage <= 0){Damage = 0;}
+
+                HP2 -= Damage;
+
+
+    }
+
+    function skills() external{
 
 
     }
@@ -30,4 +64,8 @@ interface Stat{
     function spd(uint256 PieceID) external returns(uint256);
     function maj(uint256 PieceID) external returns(uint256);
     function mhp(uint256 PieceID) external returns(uint256);
+}
+
+interface Game{
+    function hp(uint256 PieceID) external returns(uint256);
 }
