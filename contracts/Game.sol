70341f05effef9811476aac1a35dc892ff24139c
Log <91562887+LogETH@users.noreply.github.com>
2022-03-11 19:48:57 -0800

    Fixed several bugs

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -12,6 +12,14 @@ contract GameLogic {
     Movement move;
     uint GameIDNonce;
 
+    constructor(){
+
+        getstats = Stat(0x16B20c9Db9c99669dF97498B6Dab1f04fae0dbab);
+        combat = CombatContract(0x742Fb9C053Eb756E98d36C6859DfAACe581Ce0C0);
+        move = Movement(0x4b449FEB7D635862Ab07C86C0eA3b6018EC4a3A7);
+    }
+
+
 ////Nested mappings are bruh, like, very bruh
 
     mapping(address => mapping(address => uint)) Invite;
@@ -23,6 +31,7 @@ contract GameLogic {
     mapping(uint => mapping(bool => address)) WhoIsInTheGame;
     mapping(uint => Map) GameMap;
     mapping(address => bool) ingame;
+    mapping(uint => address) Turn;
 
 ////Mobile Stats
 
@@ -40,6 +49,8 @@ contract GameLogic {
         Pieces[GameIDNonce][msg.sender][4] = Piece4;
 
         WhoIsInTheGame[GameIDNonce][true] = msg.sender;
+        
+        GameMap[GameIDNonce] = EnterMapAddress;
 
         (LocationX[GameIDNonce][msg.sender][Pieces[GameIDNonce][msg.sender][1]], LocationY[GameIDNonce][msg.sender][Pieces[GameIDNonce][msg.sender][1]]) = GameMap[GameIDNonce].getspawnlocation(1);
         (LocationX[GameIDNonce][msg.sender][Pieces[GameIDNonce][msg.sender][2]], LocationY[GameIDNonce][msg.sender][Pieces[GameIDNonce][msg.sender][2]]) = GameMap[GameIDNonce].getspawnlocation(2);
@@ -47,8 +58,8 @@ contract GameLogic {
         (LocationX[GameIDNonce][msg.sender][Pieces[GameIDNonce][msg.sender][4]], LocationY[GameIDNonce][msg.sender][Pieces[GameIDNonce][msg.sender][4]]) = GameMap[GameIDNonce].getspawnlocation(4);
 
         Invite[msg.sender][Opponent] = GameIDNonce;
-        GameMap[GameIDNonce] = EnterMapAddress;
         GameIDNonce += 1;
+
     }
 
     function StartGame(uint GameID, uint Piece1, uint Piece2, uint Piece3, uint Piece4) external {
@@ -57,28 +68,32 @@ contract GameLogic {
 
         address Opponent;
 
-        require(Invite[msg.sender][Opponent] == GameIDNonce);
+        require(Invite[msg.sender][Opponent] == GameID);
 
         Pieces[GameID][msg.sender][1] = Piece1;
         Pieces[GameID][msg.sender][2] = Piece2;
         Pieces[GameID][msg.sender][3] = Piece3;
         Pieces[GameID][msg.sender][4] = Piece4;
 
-        (LocationX[GameID][msg.sender][Pieces[GameID][msg.sender][1]], LocationY[GameID][msg.sender][Pieces[GameID][msg.sender][1]]) = GameMap[GameID].getspawnlocation(1);
-        (LocationX[GameID][msg.sender][Pieces[GameID][msg.sender][2]], LocationY[GameID][msg.sender][Pieces[GameID][msg.sender][2]]) = GameMap[GameID].getspawnlocation(2);
-        (LocationX[GameID][msg.sender][Pieces[GameID][msg.sender][3]], LocationY[GameID][msg.sender][Pieces[GameID][msg.sender][3]]) = GameMap[GameID].getspawnlocation(3);
+        (LocationX[GameID][msg.sender][Pieces[GameID][msg.sender][1]], LocationY[GameID][msg.sender][Pieces[GameID][msg.sender][1]]) = GameMap[GameID].getspawnlocation(5);
+        (LocationX[GameID][msg.sender][Pieces[GameID][msg.sender][2]], LocationY[GameID][msg.sender][Pieces[GameID][msg.sender][2]]) = GameMap[GameID].getspawnlocation(6);
+        (LocationX[GameID][msg.sender][Pieces[GameID][msg.sender][3]], LocationY[GameID][msg.sender][Pieces[GameID][msg.sender][3]]) = GameMap[GameID].getspawnlocation(7);
         (LocationX[GameID][msg.sender][Pieces[GameID][msg.sender][4]], LocationY[GameID][msg.sender][Pieces[GameID][msg.sender][4]]) = GameMap[GameID].getspawnlocation(4);
 
         WhoIsInTheGame[GameID][false] = msg.sender;
 
         ingame[msg.sender] = true;
         ingame[Opponent] = true;
+        
+        Turn[GameID] = msg.sender;
     }
 
 //// Dev notes: AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 
     function PlayTurn(uint GameID, uint PieceID, int MoveHowManySpacesX, int MoveHowManySpacesY, bool Attack, uint AttackWhom) external {
 
+        require(Turn[GameID] == msg.sender, "Its not your turn!");
+        
         require (Pieces[GameID][msg.sender][1] == PieceID || Pieces[GameID][msg.sender][2] == PieceID || Pieces[GameID][msg.sender][3] == PieceID || Pieces[GameID][msg.sender][4] == PieceID, "That Piece is not currently being used in the game!");
         require (GameID < (GameIDNonce - 1), "That game doesn't even exist yet you idiot!");
         require (Dead[GameID][msg.sender][PieceID] = false, "You can't move a dead piece... its dead");
@@ -88,7 +103,7 @@ contract GameLogic {
 
         if(Attack == true){
 
-            require(move.getdistance(LocationX[GameID][msg.sender][PieceID],LocationY[GameID][msg.sender][PieceID],LocationX[GameID][this.getplayer(GameID, msg.sender)][AttackWhom],LocationY[GameID][this.getplayer(GameID, msg.sender)][AttackWhom]) == getstats.range(PieceID));
+            require(move.getdistance(LocationX[GameID][msg.sender][PieceID],LocationY[GameID][msg.sender][PieceID],LocationX[GameID][this.getplayer(GameID, msg.sender)][AttackWhom],LocationY[GameID][this.getplayer(GameID, msg.sender)][AttackWhom]) == 1, "Your range isn't far enough to hit the enemy");
 
             (CurrentHP[GameID][msg.sender][PieceID], CurrentHP[GameID][this.getplayer(GameID, msg.sender)][AttackWhom]) = combat.fight(GameID, msg.sender, this.getplayer(GameID, msg.sender), PieceID, AttackWhom);
 
@@ -102,6 +117,8 @@ contract GameLogic {
                 Dead[GameID][this.getplayer(GameID, msg.sender)][AttackWhom] = true;
             }
         }
+
+        Turn[GameID] = this.getplayer(GameID, msg.sender);
     }
 
 //// Dev notes: Using a funcation command in a nested mapping as a variable inside a nested mapping is fun
@@ -167,7 +184,7 @@ interface CombatContract{
 }
 
 interface Map{
-    function getspawnlocation(int SpawnNumber) external view returns (int, int);
+    function getspawnlocation(uint SpawnNumber) external view returns (int, int);
 
 }
 
@@ -178,7 +195,6 @@ interface Stat{
     function spd(uint256 PieceID) external returns(uint256);
     function maj(uint256 PieceID) external returns(uint256);
     function mhp(uint256 PieceID) external returns(uint256);
-    function range(uint PieceID) external returns(uint256);
 
     function ownerOf(uint256 tokenId) external view returns (address);
 }
