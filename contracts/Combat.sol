5844d99017812a2224d1649a81d6140ab1dc38ae
Log <91562887+LogETH@users.noreply.github.com>
2022-03-13 09:24:16 -0700

    Contract Update

--- contracts/Combat.sol
+++ contracts/Combat.sol
@@ -9,40 +9,50 @@ contract combat {
     Stat getstats;
     Game getdata;
 
+    constructor(){
+
+        getstats = Stat(0xC6E0041D2f62A8Ba2704B264BcE0f50E27d6ac6E);
+    }
+
+    function EditGameContract(Game Contract) external {
+
+        getdata = Contract;
+    }
+
     function fight(uint GameID, address initiator, address defender, uint Piece1, uint Piece2) external returns (uint, uint){
 
         
-        uint HP1 = getdata.hp(GameID, initiator, Piece1);
-        uint HP2 = getdata.hp(GameID, defender, Piece2);
+        int HP1 = int(getdata.hp(GameID, initiator, Piece1));
+        int HP2 = int(getdata.hp(GameID, defender, Piece2));
 
 //        this.EnablePreCombatSkills();
         
         this.Attack(Piece1, Piece2);
-        if(HP2 <= 0){return (HP1, 0);}
+        if(HP2 <= 0){return (uint(HP1), 0);}
         this.Attack(Piece2, Piece1);
-        if(HP1 <= 0){return (0, HP2);}
+        if(HP1 <= 0){return (0, uint(HP2));}
 
         if((getstats.spd(Piece1) + 10) > getstats.spd(Piece2)){
  
             this.Attack(Piece1, Piece2);
-            if(HP2 <= 0){return (HP1, 0);}
+            if(HP2 <= 0){return (uint(HP1), 0);}
         }
         if((getstats.spd(Piece2) + 10) > getstats.spd(Piece1)){
  
             this.Attack(Piece2, Piece1);
-            if(HP1 <= 0){return (0, HP2);}
+            if(HP1 <= 0){return (0, uint(HP2));}
         }
 
-        return (HP1, HP2); 
+        return (uint(HP1), uint(HP2)); 
 
     }
 
     function Attack(uint Initiator, uint Defender) external {
 
-        uint Damage;
-        uint LocalHP;
+        int Damage;
+        int LocalHP;
 
-        Damage = getstats.atk(Initiator) - getstats.def(Defender);
+        Damage = int(getstats.atk(Initiator)) - int(getstats.def(Defender));
 
 //        this.EnableInCombatSkills();
 
